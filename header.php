a794013c929d0d5acd02c7250d2959d276968fd7
Yousuf-sir <microhostbd2020@gmail.com>
2020-07-27 13:15:30 +0600

    initial commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>School Management software</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>My project</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
